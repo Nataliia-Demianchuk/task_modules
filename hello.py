fc1236fed31840edae9d771c5c10ea714b1ee2f5
Nataliia <demianchuk.n@gmail.com>
2024-05-08 11:07:31 +0200

    make files and functions

---
+++ hello.py
@@ -0,0 +1,11 @@
+# 1. Create a file called hello.py that contains a single function hello(). 
+# This function should accept a single string parameter name 
+# print the text Hello {name}! to the interactive window with {name}
+#  replaced with the function argument. 
+# Add a file called main.py that imports the hello() function from hello.py 
+# and calls the function with your name.
+
+
+def hello_funct(name):
+    print(f'Hello {name}')
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from hello import hello_funct
+
+if __name__ == '__main__':
+ hello_funct('Test')
