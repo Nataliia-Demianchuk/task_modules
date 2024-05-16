c3bfc1b926860d0fc479ac5503482a208136d794
Nataliia <demianchuk.n@gmail.com>
2024-05-16 17:45:30 +0200

    add task2 text

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ from hello import hello_funct
 
 if __name__ == '__main__':
  hello_funct('Test')
+
+
+#  2. Install the custom library          
+# 1. Create a virtual environment          
+# 2. Install numpy package in the virtual environment (version 1.22.4 or higher, but lower than 2.0.0)          
+# 3. Generate a requirements.txt file         
+# 4. Write a script that imports numpy. 
+# Execute code from their site (https://numpy.org/) and run it on a local computer          
+# 5. As a result, you should send a screenshot of the executed code on your machine, requirements.txt and a .py file
